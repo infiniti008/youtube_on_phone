5e8fa517fb7dd18d9caf80d351dfc714b06158ad
vyacheslav_nikitenok <vyacheslav.nikitenok@vsgcommerce.com>
2018-10-23 17:48:06 +0300

    sad

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var app = express()
 
-app.use(express.static('../youtube_on_phone'))
+app.use(express.static('gh-pages'))
 
 app.listen(3000, () => {
     console.log('Run in 300 port')
