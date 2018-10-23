2e5b996529ecc5047ba4d9cdb317a514ad886c91
vyacheslav_nikitenok <vyacheslav.nikitenok@vsgcommerce.com>
2018-10-23 11:30:46 +0300

    precache

--- gh-pages/scripts/sw.js
+++ gh-pages/scripts/sw.js
@@ -6,35 +6,9 @@ if (workbox) {
   console.log(`Boo! Workbox didn't load 😬`);
 }
 
-workbox.routing.registerRoute(
-    new RegExp('.*\.js'),
-    workbox.strategies.networkFirst()
-);
-
-workbox.routing.registerRoute(
-    // Cache CSS files
-    /.*\.css/,
-    // Use cache but update in the background ASAP
-    workbox.strategies.staleWhileRevalidate({
-      // Use a custom cache name
-      cacheName: 'css-cache',
-    })
-);
-
-workbox.routing.registerRoute(
-    // Cache image files
-    /.*\.(?:png|jpg|jpeg|svg|gif|ico)/,
-    // Use the cache if it's available
-    workbox.strategies.cacheFirst({
-      // Use a custom cache name
-      cacheName: 'image-cache',
-      plugins: [
-        new workbox.expiration.Plugin({
-          // Cache only 20 images
-          maxEntries: 20,
-          // Cache for a maximum of a week
-          maxAgeSeconds: 7 * 24 * 60 * 60,
-        })
-      ],
-    })
-);
+workbox.precaching.precacheAndRoute([
+    'gh-pages/styles/style.css',
+    'gh-pages/scripts/index.js',
+    'gh-pages/favicon.ico',
+    'gh-pages/index.html',
+]);
